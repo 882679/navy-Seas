5090f584c7c4f5c8d5a1f5fba52c0a730a034ea0
Angelo Convertini <871132@stud.unive.it>
2022-04-01 16:27:09 +0200

    Aggiunto Collection.java
    Aggiunta classe Collection che permette di fare operazioni CRUD sulla/e collection/s

--- app/src/main/java/com/example/navyseas/database/Database.java
+++ app/src/main/java/com/example/navyseas/database/Database.java
@@ -16,32 +16,6 @@ public class Database {
         return MongoClients.create(connectionString).getDatabase("Navy-SEAS");
     }
 
-    public static void populateCollection(MongoCollection<Document> collection, Document doc) {
-        try {
-            InsertOneResult result = collection.insertOne(doc);
-            System.out.println("Success! Inserted document id: " + result.getInsertedId());
-        } catch (MongoException me) {
-            System.err.println("Unable to insert due to an error: " + me);
-        }
-    }
-
-    public static void listAllDocumentsFromCollection(MongoCollection<Document> collection) {
-        try {
-            collection.find().forEach(System.out::println);
-        } catch (MongoException me) {
-            System.err.println("Unable to delete due to an error: " + me);
-        }
-    }
-
-    public static void deleteAllDocumentsFromCollection(MongoCollection<Document> collection) {
-        try {
-            DeleteResult result = collection.deleteMany(new Document());
-            System.out.println("Deleted document count: " + result.getDeletedCount());
-        } catch (MongoException me) {
-            System.err.println("Unable to delete due to an error: " + me);
-        }
-    }
-
     public static void deleteAllDocuments(MongoDatabase db) {
         // Deletes all documents in the database
         db.listCollectionNames().forEach(collection -> {
